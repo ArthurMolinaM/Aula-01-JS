75e3ee8ee43127457f1964e7c0b997b236d83ef5
Arthur Molina <gitarthurmolina@gmail.com>
2025-02-10 10:41:28 -0300

    01 Criando o arquivo index.html e o arquivo inicial de js

---
+++ js/base.js
@@ -0,0 +1,30 @@
+// Exibir o console
+// console só existe no navegador
+console.log('Olá, sou o console');
+console.log('Eu exibo conteúdo');
+
+// variáveis
+let rmAluno = 666;
+let nomeAluno = 'Rivaldo Jesus';
+let turmaAluno = '3TI';
+const ESCOLA_ALUNO = 'Lourdão';
+
+console.log(rmAluno, nomeAluno);
+console.log(turmaAluno, ESCOLA_ALUNO);
+
+// concatenação - pessima pratica, mas muito comum
+console.log('Nome do aluno: ' + nomeAluno + ' RM do aluno: ' + rmAluno);
+
+// Template String
+console.log(`Nome do Aluno ${nomeAluno} Rm do aluno ${rmAluno}`);
+
+turmaAluno = '3A';
+console.log(`Turma do aluno: ${turmaAluno}`);
+
+turmaAluno = '3AC';
+console.log(`Turma do aluno: ${turmaAluno}`);
+
+
+  
+
+
